4933f04137be90af9bdedcb53e775745dcc56363
amateurdullahan <zabrak12@gmail.com>
2020-09-17 19:29:19 +0000

    1

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -6,3 +6,12 @@ from requests import get
 def top_ten(subreddit):
     """return top ten in subreddit"""
     rurl = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
+    headers = {"User-Agent": ''}
+    params = {"limit": 10}
+    request = get(rurl, headers=headers, params=params, allow_redirects=False)
+    if request.status_code == 404:
+        print("None")
+        return
+    results = request.json().get("data")
+    for result in results.get("children"):
+        print(result.get("data").get("title"))
