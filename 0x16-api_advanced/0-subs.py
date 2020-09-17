655f16c23c9dc91280e5cbeeaf7c3a1ca8dff3cb
amateurdullahan <zabrak12@gmail.com>
2020-09-17 18:14:08 +0000

    0

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -6,3 +6,9 @@ from requests import get
 def number_of_subscribers(subreddit):
     """return number of subs in subreddit"""
     rurl = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    req = get(rurl, allow_redirects=False, headers={'User-agent': ''})
+    if req.json().get("kind") == "t5":
+        data = req.json().get("data")
+        return data.get("subscribers")
+    else:
+        return 0
